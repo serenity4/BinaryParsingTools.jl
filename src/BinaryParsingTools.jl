9acc9e19df21fe0d1419e191d570862e3e8c9728
serenity4 <cedric.bel@hotmail.fr>
2024-06-05 21:24:35 +0200

    Port files from OpenType.jl

--- src/BinaryParsingTools.jl
+++ src/BinaryParsingTools.jl
@@ -1,5 +1,33 @@
 module BinaryParsingTools
 
-# Write your package code here.
+using SwapStreams: SwapStream
+using .Meta: isexpr
+
+swap_endianness(io::IO, ::Type{T}) where {T} = false
+
+function read_binary end
+
+read(io::IOBuffer, ::Type{T}) where {T} = read_binary(SwapStream(swap_endianness(io, T), io), T)
+read(io::IO, ::Type{T}) where {T} = read(IOBuffer(Base.read(io)), T)
+
+"""
+Read a value of type `T` located at an offset from a given start (defaulting
+to the current position), without modifying the stream position.
+"""
+function read_at(io::IO, @nospecialize(T), offset, args...; start = position(io))
+  pos = position(io)
+  seek(io, start + offset)
+  val = read(io, T, args...)
+  seek(io, pos)
+  val
+end
+
+include("serializable.jl")
+include("tag.jl")
+
+export read_at, @serializable, Tag, @tag_str, Tag2, Tag3, Tag4
+
+# XXX: seems to crash CSTParser for VSCode linting
+# public read, swap_endianness, read_binary
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using BinaryParsingTools
 using Test
 
 @testset "BinaryParsingTools.jl" begin
-    # Write your tests here.
-end
+  @testset "Tags" begin
+    t = Tag("FRA ")
+    @test isa(t, Tag4)
+    @test convert(Tag, "FRA ") === t
+    @test convert(Tag4, "FRA ") === t
+    @test tag"FRA " === t
+    @test tag"FRA" !== tag"FRA "
+    @test isa(tag"FRA", Tag3)
+    @test_throws "4-character" Tag4("FRA")
+    @test_throws "ASCII" Tag("FRAα")
+    @test uppercase(tag"fr") === tag"FR"
+    @test lowercase(tag"FR") === tag"fr"
+  end
+end;
